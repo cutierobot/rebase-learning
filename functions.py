7348e1defd503ea7bddcc66d2676211cebc89d9a
cutierobot <20594067+cutierobot@users.noreply.github.com>
2025-06-25 18:50:32 +1000

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+"""
+The overall idea of this file is through in few intentinal issues, leave some room for merge conflits
+create some functions that need definitions that need rebasing. allow for a bit of fiddle.
+
+Basically so I don't have a blank canvas
+"""
+
+
+def add(num1, num2):
+# TODO: add function definition eventually
+  return num1 + num2
+
+def submarine(num1, numb2):
+  return num2 - num1
+
